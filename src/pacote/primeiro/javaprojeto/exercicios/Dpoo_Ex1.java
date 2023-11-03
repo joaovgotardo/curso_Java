0c9ce77b26d6d5e5cbb7be84c968cdc705027856
Joao Vitor <jvitorgotardo0514@gmail.com>
2023-11-03 15:22:23 -0300

    Adiciona os arquivos restantes sobre polimorfismo

---
+++ src/pacote/primeiro/javaprojeto/exercicios/Dpoo_Ex1.java
@@ -0,0 +1,4 @@
+package pacote.primeiro.javaprojeto.exercicios;
+
+public class Dpoo_Ex1 {
+}
--- src/pacote/primeiro/javaprojeto/javacore/Lpolimorfismo/teste/ProdutoTeste.java
+++ src/pacote/primeiro/javaprojeto/javacore/Lpolimorfismo/teste/ProdutoTeste.java
@@ -1,5 +1,6 @@
 package pacote.primeiro.javaprojeto.javacore.Lpolimorfismo.teste;
 
+import pacote.primeiro.javaprojeto.javacore.Lpolimorfismo.dominio.Celular;
 import pacote.primeiro.javaprojeto.javacore.Lpolimorfismo.dominio.Computador;
 import pacote.primeiro.javaprojeto.javacore.Lpolimorfismo.dominio.Notebook;
 import pacote.primeiro.javaprojeto.javacore.Lpolimorfismo.servico.CalcularTaxa;
@@ -8,7 +9,9 @@ public class ProdutoTeste {
     public static void main(String[] args) {
         Computador computador = new Computador("SK1i5", 7000);
         Notebook notebook = new Notebook("ASUSi7", 6500);
-        CalcularTaxa.CalculaTaxaComputador(computador);
-        CalcularTaxa.CalculaTaxaNotebook(notebook);
+        Celular celular = new Celular("SAMSUNG-A10", 2000);
+        CalcularTaxa.CalculaTaxa(computador);
+        CalcularTaxa.CalculaTaxa(notebook);
+        CalcularTaxa.CalculaTaxa(celular);
     }
 }
