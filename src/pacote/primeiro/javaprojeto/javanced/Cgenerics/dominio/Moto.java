97f32673e1536e533161b066f1f9b05ea6b733cd
Joao Vitor <jvitorgotardo0514@gmail.com>
2023-11-30 10:39:27 -0300

    Adiciona testes de classe genérica e método genérico, além de seu domínio e serviço

---
+++ src/pacote/primeiro/javaprojeto/javanced/Cgenerics/dominio/Moto.java
@@ -0,0 +1,16 @@
+package pacote.primeiro.javaprojeto.javanced.Cgenerics.dominio;
+
+public class Moto {
+    private String moto;
+
+    public Moto(String moto) {
+        this.moto = moto;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto{" +
+                "moto='" + moto + '\'' +
+                '}';
+    }
+}
