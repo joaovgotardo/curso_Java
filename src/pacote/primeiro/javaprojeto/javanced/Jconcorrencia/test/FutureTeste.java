0b343e8ac7ede30b166b44b655002520327632ab
Joao Vitor <jvitorgotardo0514@gmail.com>
2023-12-08 15:57:24 -0300

    Adiciona novos testes de Concorrência, utilizando ComparableFuture, além de um uso de ThreadFactory para um método síncrono

--- src/pacote/primeiro/javaprojeto/javanced/Jconcorrencia/test/FutureTeste.java
+++ src/pacote/primeiro/javaprojeto/javanced/Jconcorrencia/test/FutureTeste.java
@@ -1,5 +1,6 @@
 package pacote.primeiro.javaprojeto.javanced.Jconcorrencia.test;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.*;
 
 public class FutureTeste {
@@ -12,8 +13,27 @@ public class FutureTeste {
         ExecutorService es = Executors.newSingleThreadExecutor();
         //Submete uma tarefa runnable para execução.
         Future<Double> altura = es.submit(() -> {
-            TimeUnit.SECONDS.sleep(2);
+            TimeUnit.SECONDS.sleep(5);
             return 1.86D;
         });
+        horario();
+        try{
+            Double v = altura.get(4, TimeUnit.SECONDS);
+            //É ideal usar o try-catch para tratar esse get.
+            //Aqui, v retornará uma exceção se o get não for impresso nesse tempo.
+            System.out.println("Altura: " + v);
+        }catch (InterruptedException | ExecutionException | TimeoutException e){
+            e.printStackTrace();
+        }finally { //Shutdown foi posto aqui para parar sua execução. Se ocorresse uma
+            //exceção, ele não seria alcançado fora do finally.
+            es.shutdown();
+        }
     }
+    private static void horario(){
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(LocalDateTime.now());
+
+        //Parallel Streams - Usa dois núcleos para dois processos concomitantes.
+        //P. Assíncrona - Um núcleo, com processos múltiplos e não permitir sua ociosidade.
+    };
 }
